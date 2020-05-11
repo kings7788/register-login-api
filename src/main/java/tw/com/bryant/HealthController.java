9ee24d7230b2bcdd626d17e6bbd9a9f53793dd1a
bryant <bryant.chang@cloud-interactive.com>
2020-05-11 17:54:33 +0800

    test jenkins

--- src/main/java/tw/com/bryant/HealthController.java
+++ src/main/java/tw/com/bryant/HealthController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String healthCare(){
-        return "Hello project";
+        return "Hello project test jenkins";
     }
 
 }
